8eab46df9687ee8c90e9bcc9da5ed43ac751a21e
Philip Nichols <philipdnichols@gmail.com>
2013-07-29 19:09:08 -0400

    Working commit

--- testing/testing-desktop/src/com/opg/testing/Main.java
+++ testing/testing-desktop/src/com/opg/testing/Main.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "testing";
